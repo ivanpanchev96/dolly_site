6202d9886af912fdcafa14733a7140cd36b469d6
Ivan Panchev <ivan.panchev@yotpo.com>
2026-02-22 19:57:42 +0200

    change deploy folder to docs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/dolly_site/", // Change if your repo name differs (e.g. "/" for username.github.io)
+  build: {
+    outDir: "docs",
+  },
   server: {
     host: "0.0.0.0",
   },
